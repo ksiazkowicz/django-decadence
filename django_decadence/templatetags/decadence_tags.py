11a8a181647a4653c9c5992ab6776686ad0bc1b0
Maciej Janiszewski <ksiazkowicz@users.noreply.github.com>
2017-11-05 19:27:19 +0100

    don't crash if there is no request

--- django_decadence/templatetags/decadence_tags.py
+++ django_decadence/templatetags/decadence_tags.py
@@ -5,5 +5,8 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def decadence_render(context, template_name, data):
     t = template.loader.get_template(template_name)
-    data["request"] = context.request
+    try:
+        data["request"] = context.request
+    except:
+        pass
     return t.render(data)
